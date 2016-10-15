2b33e61bc9784780ae7ec652d3b497f26fccba06
antoinfive <antoinfive@gmail.com>
2016-10-15 18:26:32 -0400

    add login button

--- src/components/common/HomePage.js
+++ src/components/common/HomePage.js
@@ -1,15 +1,16 @@
 import React from 'react';
+const LEARN_ID = '0618d1cfd3af7231504179389412d85a6981781151f66c910bea0bfd73e54f93' 
+const REDIRECT_URI = 'http://localhost:5000/auth/learn/callback'
+const URL = `https://learn.co/oauth/authorize?client_id=${LEARN_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`
 
 const HomePage = () => {
   return ( 
     <div className="jumbotron">
       <h1>Hello, world!</h1>
       <p>...</p>
-      <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+      <a href={URL} className="btn btn-large"> CLICK THE THINGS </a>     
     </div>
   )
 }; 
 
 export default HomePage;
-
-
