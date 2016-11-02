837883fcbced648c2cd9438da3861033e5a7cb6a
antoinfive <antoinfive@gmail.com>
2016-11-02 10:59:57 -0400

    add modifiers for absent students and filter function to return absent days for the current user/student

--- src/components/containers/studentCalendarContainer.js
+++ src/components/containers/studentCalendarContainer.js
@@ -1,30 +1,53 @@
 import React, { Component } from 'react'
 import StudentCalendar from '../studentCalendar'
 import * as currentUserActions from '../../actions/currentUserActions'
+import * as attendanceRecordActions from '../../actions/attendanceRecordActions';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 class StudentCalendarContainer extends Component {
-  
+  constructor(){
+    super()
+    this.absentDays = this.absentDays.bind(this)
+  }
+
   componentDidMount(){
-    this.props.fetchCurrentUser();
+    if(!this.props.currentUser){
+      this.props.actions.fetchCurrentUser();
+    }
+
+    if (this.props.attendanceRecords.length == 0) {
+      this.props.actions.fetchAttendanceRecords()
+    } 
+  }
+  
+  absentDays() {
+    let dates = this.props.attendanceRecords.filter( (record) => ( record.arrived == null) )
+    return dates.map( (record) => ( new Date(record.date) )) 
   }
 
   render () {
-    return ( 
-      <StudentCalendar />      
+    const modifiers = {
+      absent: day => (this.absentDays().includes(day.toDateString()))
+    }
+    return (
+      <div> 
+        <h1> {this.absentDays()} </h1>
+      <StudentCalendar 
+        modifiers={modifiers} 
+      />
+    </div>   
     )
   }
 
 }
 
 function mapStateToProps(state, ownProps) {
-  debugger;
   return {currentStudent: state.currentUser, attendanceRecords: state.attendanceRecords }
 }
 
 function mapDispatchToProps(dispatch){
-  return bindActionCreators(currentUserActions, dispatch) 
+  return {actions: bindActionCreators(Object.assign(currentUserActions, attendanceRecordActions), dispatch)} 
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(StudentCalendarContainer)
--- src/components/studentCalendar.js
+++ src/components/studentCalendar.js
@@ -1,13 +1,16 @@
 import React from 'react' 
 import DayPicker, { DateUtils } from 'react-day-picker'
+import "../assets/calendar-style.css"
+import "../styles/absent-present-calendar.css"
 
 export default (props) => {
-
+  
   return (
     <div className='col-lg-6'>
       <DayPicker
         initialMonth={ new Date(2016, 9) }
         onDayClick={()=> { console.log("got it boss")}}
+        modifiers={props.modifiers}
       />
     </div>
 
